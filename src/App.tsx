4931a78d32ef74a2d5436887e955bf91b87b2172
Atef Ben Ali <atef.bettaib@gmail.com>
2024-06-18 12:01:29 +0100

    feat(ui): fix `app component` display

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { pdfjs, Document, Page } from "react-pdf";
 
+import "./styles.css";
+
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 export function App() {
+  const [width, setWidth] = React.useState(1200);
   const [numPages, setNumPages] = React.useState(null);
   const [pageNumber, setPageNumber] = React.useState(1);
 
@@ -24,37 +27,51 @@ export function App() {
     changePage(1);
   }
 
+  React.useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
+
   return (
-    <div
-      id="row"
-      style={{
-        height: "100vh",
-        width: "100vw",
-        display: "flex",
-        flexDirection: "column",
-        overflow: "hidden",
-      }}
-    >
+    <div id="quranContainer" className="mt-4">
       <Document
         file="https://qurancomplex.gov.sa/wp-content/uploads/isdarat/qiraat/warsh39.pdf"
         onLoadSuccess={onDocumentLoadSuccess}
       >
-        <Page pageNumber={pageNumber} />
+        <Page
+          pageNumber={pageNumber}
+          className="PDFPage PDFPageOne"
+          renderTextLayer={false}
+          renderAnnotationLayer={false}
+          scale={width > 786 ? 1.7 : 0.6}
+        />
       </Document>
-      <div>
+      <div
+        className="navigation__clz"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
         <p>
           Page {pageNumber || (numPages ? 1 : "--")} of {numPages || "--"}
         </p>
-        <button type="button" disabled={pageNumber <= 1} onClick={previousPage}>
-          Previous
-        </button>
-        <button
-          type="button"
-          disabled={pageNumber >= (numPages as unknown as number)}
-          onClick={nextPage}
-        >
-          Next
-        </button>
+        <div>
+          <button
+            type="button"
+            disabled={pageNumber <= 1}
+            onClick={previousPage}
+          >
+            {"<"}
+          </button>
+          <button
+            type="button"
+            disabled={pageNumber >= (numPages as unknown as number)}
+            onClick={nextPage}
+          >
+            {">"}
+          </button>
+        </div>
       </div>
     </div>
   );
