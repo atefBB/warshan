7d4bedc5c233cf60b34e4c80fd7138e709ee3f6a
Atef Ben Ali <atef.bettaib@gmail.com>
2024-07-09 11:20:07 +0100

    refactor: review `navigating logic`

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,53 +1,15 @@
-import { lazy, useEffect, useState } from "react";
-import { useSwipeable } from "react-swipeable";
+import { lazy } from "react";
 import { IonApp, setupIonicReact } from "@ionic/react";
-
-import { Search } from "./Search";
-import { Bookmark } from "./Bookmark";
-
-import { images as pages } from "./images";
 
 const Page = lazy(() => import("./Page"));
 
 setupIonicReact();
 
 export function Main() {
-  const lastOpenedPage = localStorage.getItem("currentPage") || 0;
-
-  const [currentPage, setCurrentPage] = useState(Number(lastOpenedPage));
-
-  useEffect(() => {
-    localStorage.setItem("currentPage", String(currentPage));
-  }, [currentPage]);
-
-  function goToNextPage() {
-    if (currentPage < pages.length - 1) {
-      setCurrentPage(currentPage + 1);
-    }
-  }
-
-  function goToPreviousPage() {
-    if (currentPage > 0) {
-      setCurrentPage(currentPage - 1);
-    }
-  }
-
-  const handlers = useSwipeable({
-    onSwipedLeft: goToPreviousPage,
-    onSwipedRight: goToNextPage,
-  });
-
   return (
     <IonApp>
-      <main {...handlers} style={{ direction: "rtl", touchAction: "pan-y" }}>
-        <Search />
-        {pages.length > 0 ? (
-          <Page
-            pageNumber={currentPage + 1}
-            imageUrl={pages[currentPage].imageUrl}
-          />
-        ) : null}
-        <Bookmark currentPage={currentPage} />
+      <main style={{ direction: "rtl", touchAction: "pan-y" }}>
+        <Page />
       </main>
     </IonApp>
   );
--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,24 +1,52 @@
+import { useEffect, useState } from "react";
 import { IonImg } from "@ionic/react";
+import { useSwipeable } from "react-swipeable";
+
+import { Search } from "./Search";
+
+import { images as pages } from "./images";
 
 import "./styles.css";
 
-type PropsType = {
-  pageNumber: number;
-  imageUrl: string;
-};
+export default function Page() {
+  const lastOpenedPage = localStorage.getItem("currentPage") || 0;
 
-export default function Page({ pageNumber, imageUrl }: PropsType) {
-  return (
-    <div className="container">
-      <header className="header"></header>
+  const [currentPage, setCurrentPage] = useState(Number(lastOpenedPage));
+
+  useEffect(() => {
+    localStorage.setItem("currentPage", String(currentPage));
+  }, [currentPage]);
+
+  function goToNextPage() {
+    if (currentPage < pages.length - 1) {
+      setCurrentPage(currentPage + 1);
+    }
+  }
+
+  function goToPreviousPage() {
+    if (currentPage > 0) {
+      setCurrentPage(currentPage - 1);
+    }
+  }
+
+  const handlers = useSwipeable({
+    onSwipedLeft: goToPreviousPage,
+    onSwipedRight: goToNextPage,
+  });
+
+  return pages.length > 0 ? (
+    <div className="container" {...handlers}>
+      <header className="search-wrapper__clz">
+        <Search />
+      </header>
       <section className="content">
         <IonImg
-          src={imageUrl}
-          alt={`Page ${pageNumber}`}
+          src={pages[currentPage].imageUrl}
+          alt={`الصفحة ${currentPage}`}
           style={{ width: "100%", height: "100%", objectFit: "fill" }}
         />
       </section>
       <footer className="page-info__clz"></footer>
     </div>
-  );
+  ) : null;
 }
